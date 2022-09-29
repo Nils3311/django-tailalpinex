d675da11c8608f2fe56ad601466857919b796b95
nils3311 <hofnil@web.de>
2022-09-29 23:12:50 +0200

    update

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
+import os
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponseNotFound
 
+testvariable = os.environ.get('test', None)
 
 def index(request):
     return render(request, "app/example.html")
@@ -10,7 +12,8 @@ def index(request):
 def index_htmx(request):
     if request.htmx:
         now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
+        # current_time = now.strftime("%H:%M:%S")
+        current_time = testvariable
         return render(request, "app/example_htmx.html", {'time': current_time})
     else:
         return HttpResponseNotFound()
