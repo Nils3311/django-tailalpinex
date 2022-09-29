dc3e716f5d8058e62291a7e2ac2239fd9b67b723
nils3311 <hofnil@web.de>
2022-09-29 23:17:15 +0200

    update

--- app/views.py
+++ app/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponseNotFound
 
-testvariable = os.environ.get('test', None)
-
 def index(request):
     return render(request, "app/example.html")
 
@@ -12,8 +10,7 @@ def index(request):
 def index_htmx(request):
     if request.htmx:
         now = datetime.now()
-        # current_time = now.strftime("%H:%M:%S")
-        current_time = testvariable
+        current_time = now.strftime("%H:%M:%S")
         return render(request, "app/example_htmx.html", {'time': current_time})
     else:
         return HttpResponseNotFound()
--- myproject/urls.py
+++ myproject/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
     path("", include('app.urls')),
 ]
+
+if os.environ.get('env', 'dev') == 'dev':
+    urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")),)
